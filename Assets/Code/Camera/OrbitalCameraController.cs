ec03ce2bf960598f890c5b7feb5c0fe54b906637
HiryuJupiter <xooase@gmail.com>
2020-11-27 23:09:04 +1100

    camera update

---
+++ Assets/Code/Camera/OrbitalCameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitalCameraController : MonoBehaviour
+{
+    [SerializeField] float panSensitivity = 0.2f;
+    [SerializeField] float tiltSensitivity = 0.2f;
+
+    new Transform camera;
+
+    float cameraTiltAngle;
+
+    void Start()
+    {
+        camera = Camera.main.transform;
+        cameraTiltAngle = camera.eulerAngles.x;
+    }
+
+    void Update()
+    {
+        float y = Input.GetAxis("Mouse X");
+        float x = Input.GetAxis("Mouse Y");
+
+        Orbit(y);
+        TiltCamera(-x);
+    }
+
+    public void Orbit(float mouseMove)
+    {
+        transform.Rotate(new Vector3(0f, mouseMove, 0f), Space.World);
+    }
+
+    void TiltCamera (float mouseMove)
+    {
+        cameraTiltAngle = Mathf.Clamp(cameraTiltAngle + mouseMove * tiltSensitivity, 30, 70);
+        //cameraTiltAngle += mouseMove;
+        camera.localRotation = Quaternion.Euler(cameraTiltAngle, 0f, 0f); //Tilt
+    }
+
+    //void OnGUI()
+    //{
+    //    GUI.Label( new Rect(20, 20, 600, 20), "camera.eulerAngles: " + camera.eulerAngles);
+    //    GUI.Label( new Rect(20, 40, 600, 20), "cameraTiltAngle: " + cameraTiltAngle);
+    //}
+}
